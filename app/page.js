4b1d1392c9a74b424a0fc29fbbad6c8cd0e51d63
ps05-git <01pawansachan@gmail.com>
2026-09-04 23:09:11 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-use client";
+"use client";
 
 import { useEffect, useState } from "react";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
